da4a9350194e5ff0f87906149c19c28b0b7fe1ed
huytrg755 <huyquangtruong755@gmail.com>
2026-01-18 23:29:57 +0700

    Update FilterConfiguration - Register RequestLoggingInterceptor for API request logging

--- electro/src/main/java/com/store/electro/Middleware/FilterConfiguration.java
+++ electro/src/main/java/com/store/electro/Middleware/FilterConfiguration.java
@@ -4,16 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.store.electro.Services.UserService;
-
 @Configuration
 public class FilterConfiguration implements WebMvcConfigurer {
-
-	private final UserService userService;
-
-	public FilterConfiguration(UserService userService) {
-		this.userService = userService;
-	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
