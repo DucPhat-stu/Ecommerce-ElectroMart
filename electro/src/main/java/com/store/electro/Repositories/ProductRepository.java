be2595e540191733cf887de6e32a4518d7617b82
Chau The Tung <tungc171@gmail.com>
2026-01-19 19:55:43 +0700

    Update entities, controller, repository and config

--- electro/src/main/java/com/store/electro/Repositories/ProductRepository.java
+++ electro/src/main/java/com/store/electro/Repositories/ProductRepository.java
@@ -6,25 +6,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-<<<<<<< HEAD
-
-import com.store.electro.Models.Entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-    @Query("""
-            SELECT DISTINCT p FROM Product p
-            LEFT JOIN FETCH p.productImages
-            WHERE p.category.id = :categoryId
-            """)
-    List<Product> findByCategoryWithImages(@Param("categoryId") Long categoryId);
-
-    @Query("""
-                SELECT p FROM Product p
-                LEFT JOIN FETCH p.productImages
-                WHERE p.id = :id
-            """)
-=======
 import org.springframework.stereotype.Repository;
 
 import com.store.electro.Models.Entity.Product;
@@ -46,7 +27,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             LEFT JOIN FETCH p.productImages
             WHERE p.id = :id
             """)
->>>>>>> af3adb4 (Update product, category, order models)
     Optional<Product> findWithImages(@Param("id") Long id);
 
 }
