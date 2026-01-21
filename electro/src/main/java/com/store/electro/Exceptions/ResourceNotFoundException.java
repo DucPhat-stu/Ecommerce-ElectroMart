4a539080fad8093120f0c278614076abf444cf81
nkhoa <nkhoa3625@gmail.com>
2026-01-21 20:11:00 +0700

    Add ResourceNotFoundException and ApiResponse classes for improved error handling and response structure

---
+++ electro/src/main/java/com/store/electro/Exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.store.electro.Exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
